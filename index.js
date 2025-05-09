1b0c130d01b65bec8852f5f8cde15b0c7d8d31cd
eehab-saadat <eehab.saadat@gmail.com>
2025-05-09 12:42:23 +0500

    added a user route

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const logger = require("./logger");
 
 const app = express();
+app.use(logger);
+
+const users = [
+    { id: 1, name: "John", age: 20 },
+    { id: 2, name: "Jane", age: 21 },
+    { id: 3, name: "Jim", age: 22 },
+];
 
 app.get("/", (req, res) => {
     res.send("Hello World");
+});
+
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.name} (${user.age})</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
 });
 
 app.listen(3000, () => {
